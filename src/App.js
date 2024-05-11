fe8178ce2d416554d4da3ff3443ea420fee55ccc
freetimezp <kraftukrnet@ukr.net>
2024-05-11 20:03:45 +0300

    02. js-portfolio-83-web-dev. Main sections. css.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+import Navbar from "./components/navbar/Navbar";
+import Header from "./components/header/Header";
+import About from "./components/about/About";
+import Services from "./components/services/Services";
+import Skills from "./components/skills/Skills";
+import Projects from "./components/projects/Projects";
+import Testimonial from "./components/testimonial/Testimonial";
+import Contact from "./components/contact/Contact";
+import Footer from "./components/footer/Footer";
+
 function App() {
   return (
     <>
-      App
+      <Navbar />
+      <Header />
+      <About />
+      <Services />
+      <Skills />
+      <Projects />
+      <Testimonial />
+      <Contact />
+      <Footer />
     </>
   );
 }
